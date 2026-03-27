379213eaba93f0783d676d29ccf0b828dba16f2a
abdusaziz <abdusaziz@gmail.com>
2026-03-27 21:41:49 +0600

    Admin panel Products Page complete

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,63 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+use Illuminate\Support\Facades\Auth;
 
 class Product extends Model
 {
-    //
+    use HasSlug, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'published',
+        'inStock',
+        'price',
+        'quantity',
+        'category_id',
+        'brand_id',
+        'created_by',
+        'updated_by',
+        'deleted_by',
+    ];
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
+
+    public function product_images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function ($product) {
+            if (Auth::check()) {
+                $product->deleted_by = Auth::id();
+                $product->save();
+            }
+        });
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\AdminAuthController;
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\ProductController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -27,6 +28,13 @@ Route::group(['prefix' => 'admin','middleware' => 'redirectAdmin'], function () 
 
 Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
     Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+
+    // Product Routes
+    Route::get('/products', [ProductController::class, 'index'])->name('admin.products.index');
+    Route::post('/products/store', [ProductController::class, 'store'])->name('admin.products.store');
+    Route::patch('/products/update/{id}', [ProductController::class, 'update'])->name('admin.products.update');
+    Route::delete('/products/delete/{id}', [ProductController::class, 'destroy'])->name('admin.products.delete');
+    Route::delete('/products/image/delete/{id}', [ProductController::class, 'deleteImage'])->name('admin.products.image.delete');
 });
 
 
