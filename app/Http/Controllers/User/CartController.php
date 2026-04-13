a3f918bfe417935bd2a98c73a6e045b87f07b15c
abdusaziz <abdusaziz@gmail.com>
2026-04-13 06:45:48 +0600

    vid 10 m36

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\User;
 
+use App\Helper\Cart;
 use App\Http\Controllers\Controller;
+use App\Models\CartItem;
 use App\Models\Product;
 use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
@@ -15,34 +17,93 @@ class CartController extends Controller
         // Logic to display the cart view
     }
 
-    public function store(Request $request,Product $product)
+    public function store(Request $request, Product $product)
     {
         $quantity = $request->post('quantity', 1);
         $user = $request->user();
 
-        if($user){
+        if ($user) {
             $cart = $user->cart()->where('product_id', $product->id)->first();
-            if($cart){
+            if ($cart) {
                 $cart->increment('quantity');
-            }else{
+            } else {
                 $user->cart()->create([
                     'product_id' => $product->id,
                     'quantity' => 1,
                 ]);
             }
-        }else{
-            // Handle the case when the user is not authenticated
+        } else {
+            $cartItems = Cart::getCookieCartItems();
+            $isProductExist = false;
+            foreach ($cartItems as &$cartItem) {
+                if ($cartItem['product_id'] == $product->id) {
+                    $cartItem['quantity'] += $quantity;
+                    $isProductExist = true;
+                    break;
+                }
+            }
+            if (!$isProductExist) {
+                $cartItems[] = [
+                    'user_id' => null,
+                    'product_id' => $product->id,
+                    'quantity' => $quantity,
+                    'price' => $product->price,
+                ];
+                Cart::setCookieCartItems($cartItems);
+            }
+            return redirect()->back()->with('success', 'Product added to cart successfully!');
         }
     }
 
-    public function update(Request $request, $cartItemId)
+    public function update(Request $request, Product $product)
     {
-        dd($request->all());
-        // Logic to update the quantity of a cart item
+        $quantity = $request->post('quantity', 1);
+        $user = $request->user();
+        if ($user) {
+            $cartItem = $user->cart()->where('product_id', $product->id)->first();
+            if ($cartItem) {
+                $cartItem->update(['quantity' => $quantity]);
+            }
+        } else {
+            $cartItems = Cart::getCookieCartItems();
+            foreach ($cartItems as &$cartItem) {
+                if ($cartItem['product_id'] == $product->id) {
+                    $cartItem['quantity'] = $quantity;
+                    break;
+                }
+            }
+            Cart::setCookieCartItems($cartItems);
+        }
+        return redirect()->back()->with('success', 'Cart updated successfully!');
     }
 
-    public function destroy($cartItemId)
+    public function destroy(Request $request, Product $product)
     {
-        // Logic to remove an item from the cart
+        $user = $request->user();
+        if ($user) {
+            $cartItem = $user->cart()->where('product_id', $product->id)->first();
+            if ($cartItem) {
+                $cartItem->delete();
+                if(CartItem::count() <= 0){
+                    return redirect()->route('home')->with('info', 'Cart is empty!');
+                }else{
+                    return redirect()->back()->with('success', 'Product removed from cart successfully!');
+                }
+            }
+        } else {
+            $cartItems = Cart::getCookieCartItems();
+            foreach ($cartItems as $key => $cartItem) {
+                if ($cartItem['product_id'] == $product->id) {
+                    unset($cartItems[$key]);
+                    break;
+                }
+            }
+        }
+        Cart::setCookieCartItems($cartItems);
+        if(count($cartItems) <= 0){
+            return redirect()->route('home')->with('info', 'Cart is empty!');
+        }else{
+            return redirect()->back()->with('success', 'Product removed from cart successfully!');
+        }
     }
 }
